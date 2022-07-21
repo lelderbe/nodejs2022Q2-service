9494cdc0c1e600e74a1b00858b78c15b62679fc7
lelderbe <fvdc@ya.ru>
2022-07-21 18:19:42 +0300

    feat: add typeorm to module artists

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -4,9 +4,12 @@ import { ArtistsController } from './artists.controller';
 import { AlbumsModule } from '../albums/albums.module';
 import { TracksModule } from '../tracks/tracks.module';
 import { FavoritesModule } from '../favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from './entities/artist.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Artist]),
     forwardRef(() => AlbumsModule),
     forwardRef(() => TracksModule),
     forwardRef(() => FavoritesModule),
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -4,42 +4,37 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import * as uuid from 'uuid';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { Artist } from './entities/artist.entity';
 import { Errors } from '../app/constants';
 import { AlbumsService } from '../albums/albums.service';
 import { TracksService } from '../tracks/tracks.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ArtistsService {
-  private readonly artists = [];
-
   constructor(
     @Inject(forwardRef(() => AlbumsService))
     private readonly albumsService: AlbumsService,
     @Inject(forwardRef(() => TracksService))
     private readonly tracksService: TracksService,
+    @InjectRepository(Artist)
+    private readonly artistsRepository: Repository<Artist>,
   ) {}
 
   async create(input: CreateArtistDto): Promise<Artist> {
-    const artist = Object.assign(new Artist(), {
-      id: uuid.v4(),
-      ...input,
-    });
-
-    this.artists.push(artist);
-
-    return artist;
+    const artist = this.artistsRepository.create(input);
+    return this.artistsRepository.save(artist);
   }
 
   async findAll(): Promise<Artist[]> {
-    return this.artists;
+    return this.artistsRepository.find();
   }
 
   async findOne(id: string): Promise<Artist> {
-    const artist = this.artists.find((item) => item.id === id);
+    const artist = this.artistsRepository.findOneBy({ id });
 
     if (!artist) {
       throw new NotFoundException(Errors.ARTIST_NOT_FOUND);
@@ -49,19 +44,12 @@ export class ArtistsService {
   }
 
   async update(id: string, input: UpdateArtistDto): Promise<Artist> {
-    return Object.assign(await this.findOne(id), input);
+    const artist = Object.assign(await this.findOne(id), input);
+    return this.artistsRepository.save(artist);
   }
 
   async remove(id: string): Promise<Artist> {
-    const index = this.artists.findIndex((item) => item.id === id);
-
-    if (index != -1) {
-      await this.albumsService.removeArtist(id);
-      await this.tracksService.removeArtist(id);
-
-      return this.artists.splice(index, 1)[0];
-    }
-
-    throw new NotFoundException(Errors.ARTIST_NOT_FOUND);
+    const artist = await this.findOne(id);
+    return this.artistsRepository.softRemove(artist);
   }
 }
